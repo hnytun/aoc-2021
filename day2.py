770ed5d46923650b8e8cb9b55dfbe012eb3694c5
Henrik Nytun <henrik.nytun@tietoevry.com>
2021-12-02 10:09:58 +0100

    cleaned up code

--- day2.py
+++ day2.py
@@ -1,8 +1,6 @@
 file1 = open('input/input2', 'r')
 lines = file1.read().splitlines()
 
-print(lines)
-#part 1 and 2, commented lines is part 1
 horizontal=0
 depth=0
 aim=0
@@ -13,13 +11,8 @@ for line in lines:
     if(direction=="forward"):
         horizontal+=distance
         depth+=aim*distance
-        print("added " + str(distance) + " to horizontal")
     elif(direction=="up"):
-        #depth-=distance
         aim-=distance
-        print("decreased " + str(distance) + " to depth")
     elif(direction=="down"):
-        #depth+=distance
         aim+=distance
-        print("added " + str(distance) + " to depth")
 print(horizontal*depth)
