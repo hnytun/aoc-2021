ad4639b45c83a352e9649b9c5d5c8fd0c57270e4
hnytun <hnytun@gmail.com>
2021-12-05 13:17:49 +0100

    finished day 3

--- day3.py
+++ day3.py
@@ -1,10 +1,10 @@
 from collections import Counter
-file1 = open('input/input3_Sample', 'r')
+file1 = open('input/input3', 'r')
 
 lines = file1.read().splitlines()
 lines2 = lines.copy()
 
-def GetGamma(list,epsilon):
+def GetGamma(list):
     lineLength = len(list[0])
     result=""
     currentIndex=0
@@ -12,7 +12,7 @@ def GetGamma(list,epsilon):
         ones=0
         zeroes=0
         for line in list:
-            if(line[i]) in "1":
+            if(line[i] in "1"):
                 ones+=1
             else:
                 zeroes+=1
@@ -21,29 +21,45 @@ def GetGamma(list,epsilon):
         elif(zeroes>ones):
             result+="0"
         else:
-            if(not epsilon):
-                result+="1"
+            result+="1"
+    return result
+
+def GetEpsilon(list):
+
+    lineLength = len(list[0])
+    result=""
+    currentIndex=0
+    for i in range(0,lineLength):
+        ones=0
+        zeroes=0
+        for line in list:
+            if(line[i] in "1"):
+                ones+=1
             else:
-                result+="0"
-    if(not epsilon):
-        return result
-    else:
-        return ''.join('1' if x == '0' else '0' for x in result)
+                zeroes+=1
+        if(ones>zeroes):
+            result+="0"
+        elif(zeroes>ones):
+            result+="1"
+        else:
+            result+="0"
+    return result #01010
 
-print(GetGamma(lines,True)) #gamma
-print(GetGamma(lines,False)) #epsilon
+for j in range(0,12):
+    lines[:] = [x for x in lines if x[j] == GetGamma(lines)[j]]
+
+for i in range(0,12):
+    lines2[:] = [x for x in lines2 if x[i] == GetEpsilon(lines2)[i]]
+    if(len(lines2) == 1 ):
+        break
+
+print(int(lines[0],2)*int(lines2[0],2))
 
 
-while(len(lines)!=1):
-    for j in range(0,5):
-        for line in lines:
-            if(line[j] != GetGamma(lines,False)[j]):
-                lines.remove(line)
 
-while(len(lines2)!=1):
-    for j in range(0,5):
-        print("most common on index " + str(j) +" is " + GetGamma(lines,False)[j])
-        for line in lines2:
-            print(lines2)
-            if(line[j] == GetGamma(lines,False)[j]):
-                lines2.remove(line)
+
+
+#print(lines2)
+#print(GetGamma(["00100", "11110", "10110", "11111"]))
+#print(GetEpsilon(["00100", "11110", "10110", "11111"]))
+
